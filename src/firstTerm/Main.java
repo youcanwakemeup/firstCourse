84f68cca9b4868f11067f775ce7db50f5e6c829c
shard <alisher.sharipov2@mail.ru>
2023-04-03 03:39:22 +0600

    Basic difficulty done

--- src/firstTerm/Main.java
+++ src/firstTerm/Main.java
@@ -1,6 +1,44 @@
 package firstTerm;
 
 public class Main {
+    public static void printAllEmployeesInfo(Employee[] repository) {
+        for (Employee employee : repository) {
+            System.out.println(employee);
+        }
+    }
+    public static int totalSalary(Employee[] repository) {
+        int totalSalary = 0;
+        for (Employee salary : repository) {
+            totalSalary += salary.getSalary();
+        }
+        return totalSalary;
+    }
+    public static Employee employeeWithMinSalary (Employee[] repository) {
+        Employee minSalary = repository[0];
+        for (Employee salary : repository) {
+            if (minSalary.getSalary() > salary.getSalary()) {
+                minSalary = salary;
+            }
+        }
+        return minSalary;
+    }
+    public static Employee employeeWithMaxSalary (Employee[] repository) {
+        Employee maxSalary = repository[0];
+        for (Employee salary : repository) {
+            if (maxSalary.getSalary() < salary.getSalary()) {
+                maxSalary = salary;
+            }
+        }
+        return maxSalary;
+    }
+    public static int avgSalary(Employee[] repository) {
+        return totalSalary(repository) / repository.length;
+    }
+    public static void printAllNames(Employee[] repository) {
+        for (Employee name : repository) {
+            System.out.println(name.getFullName());
+        }
+    }
     public static void main(String[] args) {
         Employee[] repository = new Employee[10];
         repository[0] = new Employee("Ivan Ivanov", 1, 100_000);
@@ -13,31 +51,12 @@ public class Main {
         repository[7] = new Employee("Evgenii Voronov", 3, 85_000);
         repository[8] = new Employee("Andrei Zaitsev", 4, 110_000);
         repository[9] = new Employee("Vitaly Belov", 5, 130_000);
-        int totalSalary = 0;
-        Employee minSalary = repository[0];
-        Employee maxSalary = repository[0];
-        for (Employee element : repository) {
-            System.out.println(element.toString());
-        }
-        for (Employee salary : repository) {
-            totalSalary += salary.getSalary();
-        }
-        System.out.println("Сумма затрат на зарплаты в месяц: " + totalSalary);
-        for (Employee employee : repository) {
-            if (minSalary.getSalary() > employee.getSalary()) {
-                minSalary = employee;
-            }
-        }
-        System.out.println("Работник с наименьшей зарплатой: " + minSalary);
-        for (Employee employee : repository) {
-            if (maxSalary.getSalary() < employee.getSalary()) {
-                maxSalary = employee;
-            }
-        }
-        System.out.println("Работник с наибольшей зарплатой: " + maxSalary);
-        System.out.println("Средняя зарплата: " + totalSalary/repository.length);
-        for (Employee employee : repository) {
-            System.out.println(employee.getFullName());
-        }
+
+        printAllEmployeesInfo(repository);
+        System.out.println("Сумма затрат на зарплаты в месяц: " + totalSalary(repository));
+        System.out.println("Работник с минимальной зарплатой: " + employeeWithMinSalary(repository));
+        System.out.println("Работник с максимальной зарплатой: " + employeeWithMaxSalary(repository));
+        System.out.println("Средняя зарплата: " + avgSalary(repository));
+        printAllNames(repository);
     }
 }
