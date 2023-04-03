394333978bf6205ac6e514f6043d440eb1480300
shard <alisher.sharipov2@mail.ru>
2023-04-03 18:36:11 +0600

    Medium difficulty done upd

--- src/firstTerm/Main.java
+++ src/firstTerm/Main.java
@@ -1,11 +1,12 @@
 package firstTerm;
 
 public class Main {
-    public static Employee employeeWithMinSalaryInDepartment(Employee[] repository, int department) {
+    public static Employee[] repository = new Employee[10];
+    public static Employee employeeWithMinSalaryInDepartment(int department) {
         if (department > 5 || department < 1) {
             throw new IllegalArgumentException("Выберите отдел от 1 до 5");
         }
-        Employee minSalary = employeeWithMaxSalary(repository);
+        Employee minSalary = employeeWithMaxSalary();
         for (Employee employee : repository) {
             if (employee.getDepartment() == department && employee.getSalary() < minSalary.getSalary()) {
                 minSalary = employee;
@@ -13,11 +14,11 @@ public class Main {
         }
         return minSalary;
     }
-    public static Employee employeeWithMaxSalaryInDepartment(Employee[] repository, int department) {
+    public static Employee employeeWithMaxSalaryInDepartment(int department) {
         if (department > 5 || department < 1) {
             throw new IllegalArgumentException("Выберите отдел от 1 до 5");
         }
-        Employee maxSalary = employeeWithMinSalary(repository);
+        Employee maxSalary = employeeWithMinSalary();
         for (Employee employee : repository) {
             if (employee.getDepartment() == department && employee.getSalary() > maxSalary.getSalary()) {
                 maxSalary = employee;
@@ -25,7 +26,7 @@ public class Main {
         }
         return maxSalary;
     }
-    public static double totalSalaryInDepartment(Employee[] repository, int department) {
+    public static double totalSalaryInDepartment(int department) {
         if (department > 5 || department < 1) {
             throw new IllegalArgumentException("Выберите отдел от 1 до 5");
         }
@@ -37,11 +38,11 @@ public class Main {
         }
         return totalSalary;
     }
-    public static double avgSalaryInDepartment(Employee[] repository, int department) {
+    public static double avgSalaryInDepartment(int department) {
         if (department > 5 || department < 1) {
             throw new IllegalArgumentException("Выберите отдел от 1 до 5");
         }
-        double totalSalary = totalSalaryInDepartment(repository, department);
+        double totalSalary = totalSalaryInDepartment(department);
         int peopleInDepartment = 0;
         for (Employee employee : repository) {
             if (employee.getDepartment() == department) {
@@ -50,7 +51,7 @@ public class Main {
         }
         return totalSalary / peopleInDepartment;
     }
-    public static void changeSalaryInDepartment(Employee[] repository, int department, double percentage) {
+    public static void changeSalaryInDepartment(int department, double percentage) {
         if (department > 5 || department < 1) {
             throw new IllegalArgumentException("Выберите отдел от 1 до 5");
         }
@@ -60,7 +61,7 @@ public class Main {
             }
         }
     }
-    public static void printAllEmployeesInDepartment(Employee[] repository, int department) {
+    public static void printAllEmployeesInDepartment(int department) {
         if (department > 5 || department < 1) {
             throw new IllegalArgumentException("Выберите отдел от 1 до 5");
         }
@@ -70,38 +71,38 @@ public class Main {
             }
         }
     }
-    public static void employeesWithLesserSalary(Employee[] repository, double comparingSalary) {
+    public static void employeesWithLesserSalary(double comparingSalary) {
         for (Employee employee : repository) {
             if (employee.getSalary() < comparingSalary) {
                 System.out.println(employee);
             }
         }
     }
-    public static void employeesWithGreaterSalary(Employee[] repository, double comparingSalary) {
+    public static void employeesWithGreaterSalary(double comparingSalary) {
         for (Employee employee : repository) {
             if (employee.getSalary() >= comparingSalary) {
                 System.out.println(employee);
             }
         }
     }
-    public static void changeSalary(Employee[] repository, double percentage) {
+    public static void changeSalary(double percentage) {
         for (Employee salary : repository) {
             salary.setSalary (salary.getSalary() * percentage / 100 + salary.getSalary());
         }
     }
-    public static void printAllEmployeesInfo(Employee[] repository) {
+    public static void printAllEmployeesInfo() {
         for (Employee employee : repository) {
             System.out.println(employee);
         }
     }
-    public static double totalSalary(Employee[] repository) {
+    public static double totalSalary() {
         double totalSalary = 0;
         for (Employee salary : repository) {
             totalSalary += salary.getSalary();
         }
         return totalSalary;
     }
-    public static Employee employeeWithMinSalary (Employee[] repository) {
+    public static Employee employeeWithMinSalary () {
         Employee minSalary = repository[0];
         for (Employee salary : repository) {
             if (minSalary.getSalary() > salary.getSalary()) {
@@ -110,7 +111,7 @@ public class Main {
         }
         return minSalary;
     }
-    public static Employee employeeWithMaxSalary (Employee[] repository) {
+    public static Employee employeeWithMaxSalary () {
         Employee maxSalary = repository[0];
         for (Employee salary : repository) {
             if (maxSalary.getSalary() < salary.getSalary()) {
@@ -119,16 +120,15 @@ public class Main {
         }
         return maxSalary;
     }
-    public static double avgSalary(Employee[] repository) {
-        return totalSalary(repository) / repository.length;
+    public static double avgSalary() {
+        return totalSalary() / repository.length;
     }
-    public static void printAllNames(Employee[] repository) {
+    public static void printAllNames() {
         for (Employee name : repository) {
             System.out.println(name.getFullName());
         }
     }
     public static void main(String[] args) {
-        Employee[] repository = new Employee[10];
         repository[0] = new Employee("Ivan Ivanov", 1, 100_000);
         repository[1] = new Employee("Alexander Morozov", 2, 120_000);
         repository[2] = new Employee("Danil Smirnov", 3, 200_000);
@@ -139,21 +139,20 @@ public class Main {
         repository[7] = new Employee("Evgenii Voronov", 3, 85_000);
         repository[8] = new Employee("Andrei Zaitsev", 4, 110_000);
         repository[9] = new Employee("Vitaly Belov", 5, 130_000);
-
-        /*printAllEmployeesInfo(repository);
-        System.out.println("Сумма затрат на зарплаты в месяц: " + totalSalary(repository));
-        System.out.println("Работник с минимальной зарплатой: " + employeeWithMinSalary(repository));
-        System.out.println("Работник с максимальной зарплатой: " + employeeWithMaxSalary(repository));
-        System.out.println("Средняя зарплата: " + avgSalary(repository));
-        printAllNames(repository);
-        changeSalary(repository, 15.5);
-        employeesWithLesserSalary(repository, 200_000);
-        employeesWithGreaterSalary(repository, 200_000);
-        System.out.println(employeeWithMinSalaryInDepartment(repository, 4));
-        System.out.println(employeeWithMaxSalaryInDepartment(repository, 4));
-        System.out.println(totalSalaryInDepartment(repository, 4));
-        System.out.println(avgSalaryInDepartment(repository, 4));
-        changeSalaryInDepartment(repository, 4, 15);
-        printAllEmployeesInDepartment(repository, 4);*/
+        printAllEmployeesInfo();
+        System.out.println("Сумма затрат на зарплаты в месяц: " + totalSalary());
+        System.out.println("Работник с минимальной зарплатой: " + employeeWithMinSalary());
+        System.out.println("Работник с максимальной зарплатой: " + employeeWithMaxSalary());
+        System.out.println("Средняя зарплата: " + avgSalary());
+        printAllNames();
+        changeSalary(15.5);
+        employeesWithLesserSalary( 200_000);
+        employeesWithGreaterSalary( 200_000);
+        System.out.println(employeeWithMinSalaryInDepartment( 4));
+        System.out.println(employeeWithMaxSalaryInDepartment( 4));
+        System.out.println(totalSalaryInDepartment( 4));
+        System.out.println(avgSalaryInDepartment( 4));
+        changeSalaryInDepartment( 4, 15);
+        printAllEmployeesInDepartment( 4);
     }
 }
